cd952cc8b22a94f5b8184e64e28e7ec82b76fa6b
Justin Dwyer <justindwyer6@gmail.com>
2019-10-15 12:41:30 -0700

    ruby and some mongoose

--- RubyIntroGiraffe/hello-world.rb
+++ RubyIntroGiraffe/hello-world.rb
@@ -1,6 +1,22 @@
-character_name = "Jimbob"
+character_name = "Jimbob Jones"
 character_age = "20"
 puts "Hi, my name #{character_name}."
 puts "I'm am #{character_age} years old."
 puts "You better take ol' #{character_name} to the store."
 puts "Else you'll be #{character_age} yerself."
+puts character_name.index("m")
+puts "TaCo BeLl".upcase.downcase
+
+puts ("-5 + 17" + character_age)
+def hambre
+    ["taco", "salsa", "burrito", "nacho"].each do |mexico|
+        puts "#{mexico}"
+    end
+end
+
+p hambre
+
+num = 54.5465
+puts Math.log(num)
+
+puts 10/7.0
