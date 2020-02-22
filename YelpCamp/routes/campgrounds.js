1261c2de9e99170eec42382377d24272e0cf4de6
Justin Dwyer <justindwyer6@gmail.com>
2020-02-22 15:35:20 -0800

    Finished campground authorization, starting on comment authorization

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -4,42 +4,42 @@ const express       = require("express"),
 
 // INDEX – Show campgrounds
 router.get("/", (req, res) => {
-Campground.find({}, (err, allCampgrounds) => {
-    err ? console.log(err) : res.render("campgrounds/index", {campgrounds: allCampgrounds});
-});
+    Campground.find({}, (err, allCampgrounds) => {
+        err ? console.log(err) : res.render("campgrounds/index", {campgrounds: allCampgrounds});
+    });
 });
 
 // CREATE – Add new campground to database
 router.post("/", isLoggedIn, (req, res) => {
-// Get data from form and add to campgrounds array
-const campground = {
-    name: req.body.name,
-    image: req.body.image,
-    description: req.body.description,
-    author: {
-    id: req.user._id,
-    username: req.user.username
+    // Get data from form and add to campgrounds array
+    const campground = {
+        name: req.body.name,
+        image: req.body.image,
+        description: req.body.description,
+        author: {
+        id: req.user._id,
+        username: req.user.username
+        }
     }
-}
-// Create a new campground and save to DB
-Campground.create(campground, (err, newlyCreated) => {
-    // Redirect back to campgrounds page
-    err ? console.log(err) : res.redirect("/campgrounds");
-});
+    // Create a new campground and save to DB
+    Campground.create(campground, (err, newlyCreated) => {
+        // Redirect back to campgrounds page
+        err ? console.log(err) : res.redirect("/campgrounds");
+    });
 });
 
 // NEW – Show form to create a new campground
 router.get("/new", isLoggedIn, (req, res) => {
-res.render("campgrounds/new");
+    res.render("campgrounds/new");
 });
 
 // SHOW – Show info about one campground
 router.get("/:id", (req, res) => {
-// Find campground with provided ID
-Campground.findById(req.params.id).populate("comments").exec((err, campground) => {
-    // Render show template with that campground
-    err ? console.log(err) : res.render("campgrounds/show", {campground: campground});
-});
+    // Find campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec((err, campground) => {
+        // Render show template with that campground
+        err ? console.log(err) : res.render("campgrounds/show", {campground: campground});
+    });
 });
 
 // EDIT - Show form to modify an existing campground
@@ -51,17 +51,17 @@ router.get("/:id/edit", checkCampgroundOwnership, (req, res) => {
 
 // UPDATE - Save edits made to a campground
 router.put("/:id/edit", checkCampgroundOwnership, (req, res) => {
-Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampground) => {
-    err ? res.send("Error: Go back and try again!") : res.redirect(`/campgrounds/${req.params.id}`);
-});
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampground) => {
+        err ? res.send("Error: Go back and try again!") : res.redirect(`/campgrounds/${req.params.id}`);
+    });
 });
 
 // DESTROY - Delete a campground
 router.delete("/:id", checkCampgroundOwnership, (req, res) => {
-Campground.findById(req.params.id, (err, campground) => {
-    campground.deleteOne();
-    res.redirect("/campgrounds");
-});
+    Campground.findById(req.params.id, (err, campground) => {
+        campground.deleteOne();
+        res.redirect("/campgrounds");
+    });
 });
 
 // ==========
@@ -76,29 +76,15 @@ function isLoggedIn(req, res, next){
 }
 
 function checkCampgroundOwnership(req, res, next){
-    // if(req.isAuthenticated()) {
-    //     Campground.findById(req.params.id, (err, campground) => {
-    //         if(err) {
-    //             res.redirect("back");
-    //         }
-    //         else if(campground.author.id.equals(req.user._id)) {
-    //             next();
-    //         }
-    //         else {
-    //             res.redirect("back");
-    //         }
-    //     });
-    // }
-    // else {
-    //     res.redirect("back");
-    // }
     (req.isAuthenticated())
     ? Campground.findById(req.params.id, (err, campground) => {
-        (err)
-        ? res.redirect("back")
-        : (campground.author.id.equals(req.user._id))
-            ? next()
-            : res.redirect("back")
+        try {
+            (campground.author.id.equals(req.user._id))
+                ? next()
+                : res.redirect("back")
+        } catch(err) {
+            res.redirect("back")
+        }
     })
     : res.redirect("back");
 }
